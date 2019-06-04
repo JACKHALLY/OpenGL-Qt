8db06124af0090bb4fa1279a722318b75a5c2555
Jack Liu <qq1223386926@gmail.com>
2019-06-04 10:35:10 +0800

    append point and line utils to template

--- Template/OpenGL_CG_Template/common_point.h
+++ Template/OpenGL_CG_Template/common_point.h
@@ -2,18 +2,84 @@
 #define COMMON_POINT_H
 
 #include <GL/gl.h>
+#include "common_type.h"
 
-class CGPointGLint {
+template<class T>
+class CGPoint {
 public:
-    CGPointGLint(const GLint x, const GLint y);
+    CGPoint(CGType::DataType type = CGType::GL_INT_T,
+            const T x = 0, const T y = 0);
     void draw() const;
-    GLint getX() const;
-    GLint getY() const;
-    void setX(const GLint x);
-    void setY(const GLint y);
+    T getX() const;
+    T getY() const;
+    void setX(const T x);
+    void setY(const T y);
 private:
-    GLint x;
-    GLint y;
+    void drawInt() const;
+    void drawDouble() const;
+private:
+    T x;
+    T y;
+    CGType::DataType type;
 };
+
+template<class T>
+CGPoint<T>::CGPoint(CGType::DataType type, const T x, const T y)
+             :x(x), y(y), type(type)
+{}
+
+template<class T>
+void CGPoint<T>::draw() const
+{
+    switch(type)
+    {
+    case CGType::GL_INT_T:
+        drawInt();
+        break;
+    case CGType::GL_DOUBLE_T:
+        drawDouble();
+        break;
+    }
+}
+
+template<class T>
+void CGPoint<T>::drawInt() const
+{
+    glBegin(GL_POINTS);
+    glVertex2i(x, y);
+    glEnd();
+}
+
+template<class T>
+void CGPoint<T>::drawDouble() const
+{
+    glBegin(GL_POINTS);
+    glVertex2d(x, y);
+    glEnd();
+}
+
+template<class T>
+T CGPoint<T>::getX() const
+{
+    return x;
+}
+
+template<class T>
+T CGPoint<T>::getY() const
+{
+    return y;
+}
+
+template<class T>
+void CGPoint<T>::setX(const T x)
+{
+    this->x = x;
+}
+
+template<class T>
+void CGPoint<T>::setY(const T y)
+{
+    this->y = y;
+}
 
 #endif // COMMON_POINT_H
